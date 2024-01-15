9429591bd34f8e011ee3506efed80072091a9b6d
Muhammad Maaz <32775876+MaazPro@users.noreply.github.com>
2024-01-15 11:26:26 -0500

    Update index.js axios url

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ app.use(express.json());
 
 // Option 2: Allow custom origins
 app.use(cors({
-    // origin:["http://mern-bookstore-ten.vercel"],
-    origin: ["https://mern-bookstore.vercel.app"],
+    origin: ["https://mern-bookstore-ten.vercel"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
