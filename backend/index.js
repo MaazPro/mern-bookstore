c835aadd3c60a0551af30891d5c50aec1d33c2f2
Muhammad Maaz <32775876+MaazPro@users.noreply.github.com>
2024-01-15 11:27:52 -0500

    Update index.js axios url deleted

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Option 2: Allow custom origins
 app.use(cors({
-    origin: ["https://mern-bookstore-ten.vercel"],
+    origin: [""],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
 }));
